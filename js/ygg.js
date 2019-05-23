8456d1f4f74885f0c96d612df1296db86ee89436
Aymeric Bianco Pelle <biancop.aymeric@icloud.com>
2019-05-23 13:34:31 +0200

    Created js class Ygg

--- js/ygg.js
+++ js/ygg.js
@@ -1,36 +1,100 @@
-const CATEGORIES = {
-	ALL: 'all',
-	VIDEO: 2145,
-	GAMES: 2142,
-	APPS: 2144
-}
-const SUB_CATEGORIES = {
-	VIDEO : {              
-		ANIMATION: 2178,
-		ANIMATION_SERIES: 2179,
-		CONCERT: 2180,
-		DOCUMENTARY: 2181,
-		TV_SHOW: 2182,
-		FILM: 2183,
-		TV_SERIES: 2184,
-		SHOW: 2185,
-		SPORT: 2186
+class Ygg {
+
+	static ACTIONS = {
+		SEARCH: 'search'
 	}
-}
 
+	static CATEGORIES = {
+		ALL: 'all',
+		VIDEO: 2145,
+		GAMES: 2142,
+		APPS: 2144
+	}
+	static SUB_CATEGORIES = {
+		VIDEO : {              
+			ANIMATION: 2178,
+			ANIMATION_SERIES: 2179,
+			CONCERT: 2180,
+			DOCUMENTARY: 2181,
+			TV_SHOW: 2182,
+			FILM: 2183,
+			TV_SERIES: 2184,
+			SHOW: 2185,
+			SPORT: 2186
+		}
+	}
 
+	constructor() {
+		// https://www2.yggtorrent.ch/engine/search?do=search&order=desc&sort=completed
+		// https://www2.yggtorrent.ch/engine/search?&category=2145&sub_category=all&do=search
+		// https://www2.yggtorrent.ch/engine/search?name=&description=&file=&uploader=&category=2145&sub_category=2183&do=search
 
-function YggRowToObject($row) {
-	let link =  $row.find('#torrent_name');
-	let name = link.text();
-	name = name.replace(/\r?\n|\r/, '').trim();
+		$.ajax({
+			//JSONP API
+			url : '/proxy.php',
+			data: {
+				csurl: 'https://www.ygg.to'
+			},
+			dataType: 'html',
+			//work with the response
+			success: function(answer) {
+				console.log(arguments);
+			},
+			error: function() {
+				console.log(arguments);
+			}
+		});
+	}
 
-	let obj = {
-		name: name,
-		page: link.attr('href')
-	};
+	searchTorrent(query, sub_category) {
+		$.ajax({
+			//JSONP API
+			url : '/proxy.php',
+			data: {
+				csurl: 'https://www2.yggtorrent.ch/engine/search',
+				do: this.SEARCH,
+				category: this.CATEGORIES.VIDEO,
+				sub_category: sub_category,
+				order: 'desc',
+				sort: 'completed'
+			},
+			dataType: 'html',
+			//work with the response
+			success: function(answer) {
+				let $answer = $('<div></div>');
+				$answer.html(answer);
 
-	return obj;
+				let results = $answer.find('[id="#torrents"] .results tbody tr');
+
+				results.each((i, row) => )
+				console.log(this.parseRow(el.find('[id="#torrents"] .results tbody tr').first()));
+			},
+			error: function() {
+				console.log(arguments);
+			}
+		});
+	}
+
+	getTorrentInfos(id) {
+
+	}
+
+	parseRow($row) {
+		let link =  $row.find('#torrent_name');
+		let name = link.text();
+		name = name.replace(/\r?\n|\r/, '').trim();
+
+		let obj = {
+			name: name,
+			page: link.attr('href')
+		};
+
+		return obj;
+	}
+
+	parseTorrentPage() {
+
+	}
 }
 
 /*var database = (function () {
@@ -46,31 +110,4 @@ function YggRowToObject($row) {
     });
     return database;
 })();
-
-var el = $( '<div></div>' );
-
-	// https://www2.yggtorrent.ch/engine/search?do=search&order=desc&sort=completed
-	// https://www2.yggtorrent.ch/engine/search?&category=2145&sub_category=all&do=search
-	// https://www2.yggtorrent.ch/engine/search?name=&description=&file=&uploader=&category=2145&sub_category=2183&do=search
-	$.ajax({
-		//JSONP API
-		url : '/proxy.php',
-		data: {
-			csurl: 'https://www2.yggtorrent.ch/engine/search',
-			do: 'search',
-			category: CATEGORIES.VIDEO,
-			sub_category: SUB_CATEGORIES.VIDEO.FILM,
-			order: 'desc',
-			sort: 'completed'
-		},
-		dataType: 'html',
-		//work with the response
-		success: function(rawData) {
-			el.html(rawData);
-			console.log(YggRowToObject(el.find('[id="#torrents"] .results tbody tr').first()));
-		},
-		error: function(rawData) {
-			console.log(arguments);
-		}
-	});
 */
