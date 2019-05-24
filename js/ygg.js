d5d26963c12a29c55be55436ef69710db52ac0b6
Aymeric Bianco Pelle <biancop.aymeric@icloud.com>
2019-05-25 01:52:35 +0200

    Finished scrapping basis

--- js/ygg.js
+++ js/ygg.js
@@ -51,7 +51,6 @@ class Ygg {
 	}
 
 	searchTorrent(query, sub_category, callback) {
-
 		$.ajax({
 			//JSONP API
 			url : '/proxy.php',
@@ -62,7 +61,7 @@ class Ygg {
 				sub_category: sub_category,
 				sort: 'completed',
 				order: 'desc'
-			}, //  `http://www2.yggtorrent.ch/engine/search?name=${query}&do=${Ygg.SEARCH}&category=${Ygg.CATEGORIES.VIDEO}&order=desc&sort=completed$sub_category=${sub_category}`
+			},
 			dataType: 'html',
 			beforeSend: function(request) {
 				request.setRequestHeader("X-Proxy-URL", 'https://www2.yggtorrent.ch/engine/search');
@@ -72,19 +71,13 @@ class Ygg {
 				let $answer = $('<div></div>');
 				$answer.html(answer);
 
-				let container = $answer.find('[id="#torrents"]');
-				console.log($answer.find('form').serialize());
-				if(container.length > 0) {
-					let results =  container.find('tr');
-					let array = [];
-
-					results.each((i, row) => {
-						array[i] = this.parseRow($(row));
-					});
+				let table = $answer.find('[id="#torrents"] table');
+				if(table.length > 0) {
+					let array = this.parseTable(table);
 
 					callback(array);
 				} else {
-					callback([{name:'error'}]);
+					console.log('error parsing ygg');
 				}
 			},
 			error: function() {
@@ -97,21 +90,131 @@ class Ygg {
 
 	}
 
-	parseRow($row) {
-		let link =  $row.find('#torrent_name');
-		let name = link.text();
-		name = name.replace(/\r?\n|\r/, '').trim();
+	parseTable($table) {
+		let headers = [];
 
-		let obj = {
-			name: name,
-			link: link.attr('href')
+		// get table headers
+		$table.find('th').each((i, el) => {
+			let $el = $(el);
+			let name = $el.text();
+
+			// some of them are links
+			if(name.length == 0) {
+				name = $el.find('a').text();
+			}
+
+			headers.push(name);
+		});
+
+		console.log(headers);
+
+		let array = [];
+
+		// make each row an object with properties named from the headers
+		$table.find('tbody tr').each((i, el) => {
+			let cols = $(el).find('td');
+			let row = {};
+
+			// assign each td to its key
+			headers.forEach((header, i) => {
+				row[header] = cols[i];
+			});
+
+			// clean, extract and format the data
+			let parsedRow = this.parseRow(row);
+
+			array.push(parsedRow);
+		});
+
+		return array;
+	}
+
+	parseRow(rowObj) {
+		// rowObj keys : Type Nom NFO Comm. Age Taille Compl. Seed Leech
+		let $torrentLink = $(rowObj.Nom).find('a');
+		let rawName = $torrentLink.text();
+
+		let data = this.extractTags(rawName);
+
+		let row = {
+			name: data.name,
+			rawName: rawName,
+			link: $torrentLink.attr('href'),
+			age: rowObj.Age.innerHtml,
+			size: rowObj.Taille.innerHtml,
+			completed: parseInt(rowObj['Compl.'].innerHtml),
+			seed: parseInt(rowObj.Seed.innerHtml),
+			leech: parseInt(rowObj.Leech.innerHtml),
+			tags: data.tags,
+			year: data.year
 		};
 
-		return obj;
+		return row;
 	}
 
 	parseTorrentPage() {
 
+	}
+
+	extractTags(name) {
+		let data = {
+			tags: []
+		};
+
+		name = name.replaceAll(/\r?\n|\r/, '').trim()
+
+		// store year specificaly
+		let yearRegex = /\d{4}(?!p)/;
+		let yearMatch = name.match(yearRegex);
+		if(yearMatch != null) {
+			data.year = parseInt(yearMatch[0]);
+		}
+		// remove the year
+		name = name.replace(yearRegex, '');
+
+		// handle tags with dots
+		if(name.indexOf('5.1') != -1) data.tags.push('5.1');
+		if(name.indexOf('7.1') != -1) data.tags.push('7.1');
+		name = name.replaceAll(/((5\.1)|(7\.1))/, '');
+
+		// replace parentheses brackets and dots by a space
+		let rawTags = name.replaceAll(/[\[\]\(\)\.]/, ' ');
+
+		let knowTags = [
+			// resolution
+			'1080p',
+			'720p',
+			'UHD',
+
+			// language
+			'VFQ',
+			'VF2',
+			'MULTi',
+			'TRUEFRENCH',
+			'FRENCH',
+
+			// sound
+			'DTS-HD',
+
+			// codec
+			'x264-gold',
+			'x264',
+			'HEVC',
+
+			// source
+			'BluRay',
+		];
+
+		knowTags.forEach(test => {
+			if(rawTags.indexOf(test) != -1) {
+				data.tags.push(test);
+				rawTags.replaceAll(test, '');
+			}
+		});
+
+		data.name = rawTags.replace(/\s+/g, ' ');
+		console.log(data);
+		return data;
 	}
 }
 
