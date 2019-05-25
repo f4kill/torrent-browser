60c53c1515897a0706e1e7564ab9ee6604d493e1
Aymeric Bianco Pelle <biancop.aymeric@icloud.com>
2019-05-25 17:18:52 +0200

    Updated looks

--- js/main.js
+++ js/main.js
@@ -41,17 +41,31 @@ $(document).ready(function() {
 
 function setupListeners() {
 
-	$(".segmented label input[type=radio]").each(function() {
-		$(this).on("change", function() {
-			if($(this).is(":checked")) {
-			   $(this).parent().siblings().each(function() {
-					$(this).removeClass("checked");
-				});
-				$(this).parent().addClass("checked");
-			}
-		});
+	$(document).on('change', '.segmented label input[type=radio]', function() {
+		if($(this).is(':checked')) {
+		   $(this).parent().siblings().each(function() {
+				$(this).removeClass('checked');
+			});
+			$(this).parent().addClass('checked');
+		}
 	});
 
+	$(document).on('load', 'img.media-poster', () => {
+		console.log('trigger');
+		let blur = 20;
+
+		let canvas = $(this).siblings('canvas.media-card-shadow')[0];
+		if(canvas != undefined) {
+			blur *= window.devicePixelRatio;
+			let margin = blur + 2;
+			canvas.getContext("2d").drawImage(this, margin, margin, canvas.width - margin*2, canvas.height - margin*2);
+
+			stackBlurCanvasRGBA(canvas, 0, 0, canvas.width, canvas.height, blur);
+		}
+	}).each(function() {
+		console.log('complete', this)
+		if(this.complete) $(this).trigger('load');
+	});
 
 
 
@@ -92,6 +106,19 @@ function setupListeners() {
 	// });
 }
 
+function imageToBlurredCanvas(image, blur) {
+	let $image = $(image);
+
+	var canvas = document.createElement("canvas");
+	blur *=  window.devicePixelRatio;
+	canvas.width = image.offsetWidth + blur * 2 + 4;
+	canvas.height = image.offsetHeight + blur * 2 + 4;
+	canvas.getContext("2d").drawImage(image, blur + 2, blur + 2, image.width, image.height);
+
+	stackBlurCanvasRGBA(canvas, 0, 0, canvas.width, canvas.height, blur);
+
+	return canvas;
+}
 
 function requestTMDb(callback, request = { method: "discover", media_type: "movie" } ) {
 	// if(api.url == undefined || api.version == undefined || api.key == undefined || request.method == undefined) {
