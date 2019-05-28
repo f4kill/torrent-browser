d1085d4834bf1e8cdaf05abcbdbb2c0d0f36b962
Aymeric Bianco Pelle <biancop.aymeric@icloud.com>
2019-05-28 18:15:51 +0200

    Fixed indent

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    
-	<section class="content">
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+
+		<section class="content">
 		<!-- <Filters/> -->
-		<Feed/>
-	</section>
-  </div>
+			<Feed/>
+		</section>
+	</div>
 
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
-import Filters from '@/components/Filters.vue';
 import Feed from '@/components/Feed.vue';
 
 export default {
 	name: 'home',
 	components: {
 		HelloWorld,
-		Filters,
 		Feed,
 	},
 };
