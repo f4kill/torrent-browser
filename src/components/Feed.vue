b48413f6b05b92bfab5bec9375bd7142e2aaa8a6
Aymeric Bianco Pelle <biancop.aymeric@icloud.com>
2019-05-29 16:21:09 +0200

    Fixed template and scss

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,8 +1,8 @@
 <template>
-	<div v-if="medias" class="feed">
+	<div v-if="medias.length" class="feed">
 		<article v-for="media in medias" :key="media.id" class="media-card">
 			<section class="content">
-				<img class="owned" src="img/owned.svg"/>
+				<img class="owned-icon" src="img/owned.svg"/>
 				<div class="infos">
 					<h1 class="title" :title="media.title">{{ media.title }}</h1>
 					<h2 class="release-year">{{ media.release_year }}</h2>
@@ -47,9 +47,13 @@ import { mapState } from 'vuex';
 export default {
 	name: 'Feed',
 
-	computed: mapState([
-		'medias',
-	]),
+	data() {
+		return {
+			isOpen: false,
+		};
+	},
+
+	computed: mapState(['medias']),
 
 	created() {
 		this.$store.dispatch('refresh');
@@ -65,6 +69,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.feed {
+	display: flex;
+	position: relative;
+
+	flex-wrap: wrap;
+	justify-content: space-between;
+}
 
 .media-card {
 	min-width: 0;
@@ -72,156 +83,157 @@ export default {
 
 	padding: 25px;
 	transition: margin-bottom .3s ease;
+
+	.content {
+		position: relative;
+	}
+
+	.poster {
+		display: block;
+
+		width: 100%;
+		height: auto;
+
+		border-radius: 10px;
+
+		background: white;
+		/*opacity: 0;*/
+		/*visibility: hidden;*/
+	}
+
+	.shadow {
+		position: absolute;
+		top: 0;
+		left: 50%;
+		z-index: -1;
+
+		transform: translateX(-50%);
+
+		width: calc(100% + 20px);
+		height: calc(100% + 20px);
+
+		object-fit: cover;
+
+		/*filter: blur(10px);*/
+	}
+
+	&.owned {
+		.media-poster {
+			opacity: .55;
+		}
+
+		.owned-icon {
+			display: initial;
+		}
+	}
+
+	.owned-icon {
+		display: none;
+		position: absolute;
+		right: 0;
+		top: 0;
+		z-index: 1;
+
+		width: 20%;
+		height: auto;
+	}
+
+	.infos {
+		position: absolute;
+		left: 0; bottom: 0; right: 0;
+
+		padding: 20px;
+		padding-top: 60px;
+		border-radius: 0 0 10px 10px;
+
+		background-image: linear-gradient(to top, rgba(0,0,0,0.6) 0%, rgba(0,0,0,0) 100%);
+
+		opacity: 0;
+		transition: opacity .3s ease;
+	}
+
+	&:hover:not(.owned) .media-card-infos,
+	&.open:not(.owned) .media-card-infos {
+		opacity: 1;
+	}
+
+	.title,
+	.release-year {
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+
+		color: white;
+		font-size: 1.1em;
+		font-weight: 400;
+
+		margin: .5em 0;
+	}
+
+	.release-year {
+		color: #DDD;
+		margin-bottom: 0;
+	}
+
+	.title {
+		font-size: 16px;
+	}
+
+	&.open:not(.match) {
+		margin-bottom: 430px;
+	}
+
+	&.open:not(.match) .media-more {
+		height: 400px;
+	}
+
+	.more {
+		position: absolute;
+		left: 25px;
+		right: 25px;
+
+		height: 0;
+		overflow: hidden;
+
+		margin-top: 15px;
+
+		color: #3D3D3D;
+
+		transition: height .3s ease;
+
+		section {
+			position: absolute;
+			width: 100%;
+		}
+
+		table {
+			width: 100%;
+		}
+
+		.classic-results td:nth-child(3) {
+			text-align: right;
+		}
+
+		td:first-child,
+		th:first-child {
+			padding-left: 0;
+		}
+
+		td:last-child,
+		th:last-child {
+			padding-right: 0;
+		}
+
+		.download-torrent {
+			font-size: 16px;
+
+			margin: 0;
+			padding: 0;
+			border: none;
+
+			background: none;
+			cursor: pointer;
+		}
+	}
 }
-
-.media-card-content {
-	position: relative;
-}
-
-.media-poster {
-	display: block;
-
-	width: 100%;
-	height: auto;
-
-	border-radius: 10px;
-
-	background: white;
-	/*opacity: 0;*/
-	/*visibility: hidden;*/
-}
-
-.media-card-shadow {
-	position: absolute;
-	top: 0;
-	left: 50%;
-	z-index: -1;
-
-	transform: translateX(-50%);
-
-	width: calc(100% + 20px);
-	height: calc(100% + 20px);
-
-	object-fit: cover;
-
-	/*filter: blur(10px);*/
-}
-
-.media-card.match .media-poster {
-	opacity: .55;
-}
-
-.media-owned {
-	display: none;
-	position: absolute;
-	right: 0;
-	top: 0;
-	z-index: 1;
-
-	width: 20%;
-	height: auto;
-}
-
-.media-card.match .media-owned {
-	display: initial;
-}
-
-.media-card-infos {
-	position: absolute;
-	left: 0; bottom: 0; right: 0;
-
-	padding: 20px;
-	padding-top: 60px;
-	border-radius: 0 0 10px 10px;
-
-	background-image: linear-gradient(to top, rgba(0,0,0,0.6) 0%, rgba(0,0,0,0) 100%);
-
-	opacity: 0;
-	transition: opacity .3s ease;
-}
-
-.media-card:hover:not(.owned) .media-card-infos,
-.media-card.open:not(.owned) .media-card-infos {
-	opacity: 1;
-}
-
-.media-title,
-.media-year {
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-
-	color: white;
-	font-size: 1.1em;
-	font-weight: 400;
-
-	margin: .5em 0;
-}
-
-.media-year {
-	color: #DDD;
-	margin-bottom: 0;
-}
-
-.media-title {
-	font-size: 16px;
-}
-
-.media-card.open:not(.match) {
-	margin-bottom: 430px;
-}
-
-.media-card.open:not(.match) .media-more {
-	height: 400px;
-}
-
-.media-more {
-	position: absolute;
-	left: 25px;
-	right: 25px;
-
-	height: 0;
-	overflow: hidden;
-
-	margin-top: 15px;
-
-	color: #3D3D3D;
-
-	transition: height .3s ease;
-}
-
-.media-more section {
-	position: absolute;
-	width: 100%;
-}
-
-.media-more table {
-	width: 100%;
-}
-
-.classic-results td:nth-child(3) {
-	text-align: right;
-}
-
-.media-more td:first-child,
-.media-more th:first-child {
-	padding-left: 0;
-}
-
-.media-more td:last-child,
-.media-more th:last-child {
-	padding-right: 0;
-}
-
-.download-torrent {
-	font-size: 16px;
-
-	margin: 0;
-	padding: 0;
-	border: none;
-
-	background: none;
-	cursor: pointer;
-}
-
 </style>
