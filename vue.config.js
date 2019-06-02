3f74ee5a705f95b99585635f5126b2b56eda78d7
Aymeric Bianco Pelle <biancop.aymeric@icloud.com>
2019-06-02 16:18:22 +0200

    Added api proxies to vue config

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,27 @@ module.exports = {
 	devServer: {
 		host: 'torrent-browser.dev.local',
 		disableHostCheck: true,
+		proxy: {
+			'^/plex': {
+				target: 'http://plex.mrik.pw:32400',
+				ws: true,
+				changeOrigin: true,
+				pathRewrite: path => path.replace('/plex', '/'),
+				hostRewrite: 'torrent-browser.dev.local:8080',
+			},
+			'^/ygg': {
+				target: 'http://tbapi.dev.local',
+				ws: true,
+				changeOrigin: true,
+				pathRewrite: path => path.replace('/ygg', '/proxy.php'),
+			},
+			'^/tmdb': {
+				target: 'http://api.themoviedb.org',
+				ws: true,
+				changeOrigin: true,
+				pathRewrite: path => path.replace('/tmdb', '/'),
+				hostRewrite: 'torrent-browser.dev.local:8080',
+			},
+		},
 	},
 };
